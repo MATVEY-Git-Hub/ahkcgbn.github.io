dc0192095d6e8d6998d1c01536addf8d7b44b9c1
MATVEY <75066032+MATVEY-Git-Hub@users.noreply.github.com>
2023-01-21 13:47:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>AHK ЦГБ-Н</title>
+<meta charset="utf-8">
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="RPCS3" />
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6161192934701994" crossorigin="anonymous"></script>
+</head>
+<body>
+<img src="/img/graphics/landing/hero-main.png" width="1" height="1" alt="preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/inc-video-popout.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'xUpw9bHmoac',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'gIcsUr7W00k',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'60lq9pXzDic',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>AHK ЦГБ-Н<br>
+							 СТАНЕТ ЕЩЁ ЛУЧШЕ</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+							В 2023 году ожидается самое крупное обновление AHK ЦГБ-Н под кодовым названием «Pypet».
+							</p>
+							<div class='landing-btn-container'>
+								<div class="build-btn-button">
+									<div class="build-ico-button">
+										<div class="build-ico-os">
+											<img alt="Windows" src="img/icons/os/windows.png" style='height: 100%; width: 100%; object-fit: contain'/>
+											<img alt="Linux"   src="img/icons/os/Windows.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+											<img alt="Linux"   src="img/icons/os/Windows.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+											<img alt="FreeBSD" src="img/icons/os/Windows.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+										</div>
+									</div>
+									<a href="https://vk.com/topic-196244138_48963442">
+									<div class="build-tx1-button">
+										<span>Скачать актуальную версию</span>
+										<span class="build-tx2-label">3.0</span>
+									</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/hero-main.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
